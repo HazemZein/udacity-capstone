4d65f209673b2517fbeb33bf2ab4a37cba4dddbf
HazemZein <hazemzeinelabedeen@gmail.com>
2022-04-19 20:54:07 +0200

    adding project files

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    returnn "<p>Hello World! My name is Hazem</p>"
+    return "<p>Hello World! My name is Hazem</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
